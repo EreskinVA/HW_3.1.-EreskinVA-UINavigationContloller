2fba3e8cf55b698d2a05526504d7a747d9001c19
EreskinVA <ereskinva@icloud.com>
2019-03-05 09:32:35 +0300

    Initial Commit

---
+++ HW_3.1.-EreskinVA-UINavigationContloller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW_3.1.-EreskinVA-UINavigationContloller
+//
+//  Created by Vladimir Ereskin on 05/03/2019.
+//  Copyright © 2019 Vladimir Ereskin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
