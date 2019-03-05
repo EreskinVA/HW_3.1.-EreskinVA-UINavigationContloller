1e85734d62120ed9d28af78f9633dcd27b09adfe
EreskinVA <ereskinva@icloud.com>
2019-03-05 10:02:25 +0300

    Create LogitVC interface

---
+++ HW_3.1.-EreskinVA-UINavigationContloller/LoginVC.swift
@@ -0,0 +1,32 @@
+//
+//  LoginVC.swift
+//  HW_3.1.-EreskinVA-UINavigationContloller
+//
+//  Created by Vladimir Ereskin on 05/03/2019.
+//  Copyright © 2019 Vladimir Ereskin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    // MARK: - ... @IBOutlet
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    // MARK: - ... UIViewController Methods
+    
+    // MARK: - ... @IBAction
+
+    @IBAction func loginButton() {
+        
+    }
+    @IBAction func forgotUserNameButton() {
+        
+    }
+    
+    @IBAction func forgotPasswordButton() {
+        
+    }
+}
